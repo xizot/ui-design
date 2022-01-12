ad6887328c97dd2113d884b2cdd46d9c3d2d5360
Hieu dang <dangtrunghieu2304@gmail.com>
2022-01-13 00:40:29 +0700

    add: rated page

--- src/components/MovieItem/MovieItem.elements.jsx
+++ src/components/MovieItem/MovieItem.elements.jsx
@@ -42,7 +42,6 @@ export const MovieWrapper = styled('div')({
 });
 
 export const MovieImageWrapper = styled('div')({
-  position: 'relative',
   borderRadius: '5px 5px 0 0',
   overflow: 'hidden',
   paddingTop: 'calc(3/2 * 100%)',
@@ -103,4 +102,71 @@ export const MovieTitle = styled(Typography)({
 
 export const MovieTimer = styled(Typography)({
   fontSize: 12,
+});
+
+export const MovieHoverRated = styled(Link)({
+  position: 'absolute',
+  left: 0,
+  top: 0,
+  background: 'rgba(0,0,0,.4)',
+  margin: '60px 10px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  transition: 'all .5s',
+  opacity: 0,
+  pointerEvents: 'none',
+});
+
+export const MovieImageWrapperRated = styled('div')({
+  borderRadius: '5px 5px 0 0',
+  overflow: 'hidden',
+});
+
+export const MovieImageRated = styled('img')({
+  width: '150px',
+  height: '150px',
+  objectFit: 'cover',
+  position: 'absolute',
+  left: 0,
+  top: 0,
+});
+
+
+export const MovieWrapperRated = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  textDecoration: 'none',
+  color: '#000',
+  height: '150px',
+  border: '1px solid #DDDDDD',
+  padding: theme.spacing(0, 1.5),
+  transition: 'all .5s',
+  position: 'relative',
+  marginBottom: '10px',
+  borderRadius: '4px',
+  '& img': {
+    transition: 'all .5s',
+  },
+  '&:hover .on-hover': {
+    opacity: 1,
+    pointerEvents: 'all',
+  },
+  '&:hover .movie-title': {
+    textDecoration: 'underline',
+  },
+});
+
+export const MovieInfomation = styled('div')({
+  position: 'absolute',
+  margin: '10px 160px',
+});
+
+export const MovieStart = styled('div')({
+  display: 'flex',
+  marginRight: '5px',
+});
+
+export const MovieScoreRated = styled('div')({
+  margin: '3px 0 0 3px',
 });
--- src/pages/AUTH/Rated/Rated.jsx
+++ src/pages/AUTH/Rated/Rated.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState, useCallback, useEffect } from 'react';
+import {
+  Container,
+  styled,
+} from '@mui/material';
+import {
+  defaultSectionMargin,
+  defaultSectionMarginMobile,
+  theme,
+} from '../../../GlobalMUI';
+import {
+  listMovies,
+} from './Rated.data';
+import MovieItemRated from '../../../components/MovieItem/MovieItemRated'
+
+export const RatedSearchWrapper = styled(Container)({
+  margin: `0 auto ${defaultSectionMargin}`,
+  [theme.breakpoints.down('sm')]: {
+    margin: `0 auto ${defaultSectionMarginMobile}`,
+  },
+});
 
 function Rated() {
-  return <div>this is rated page</div>;
-}
+  const [loading, setLoading ] = useState(false)
+  const [listMovie, setListMovie ] = useState([])
+
+  const getListNewHandler = useCallback(async () => {
+    setLoading(true);
+    const fetch = await listMovies();
+    setListMovie(fetch);
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    getListNewHandler();
+  }, [
+    getListNewHandler,
+  ]);
+
+  return (
+  <RatedSearchWrapper>
+    {listMovie.map((item) => (
+      <MovieItemRated 
+        imgSrc={item.backdrop_path}
+        id={item.id}
+        title={item.title}
+        score={item.vote_average}
+      />
+    ))}
+  </RatedSearchWrapper>
+  )}
 
 export default Rated;
